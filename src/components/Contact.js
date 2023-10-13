2d3569a545c6f9c26c4f8d5d37f8dfd270467fda
kieuxuandieuhuong <115691878+kieuxuandieuhuong@users.noreply.github.com>
2023-10-13 13:20:47 +0700

    first commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,9 @@ function Contact() {
   return (
     <div>
       <h2>Contact Information</h2>
-      <p>Email: 20521331@gm.uit.edu.vn</p>
-      <p>Address: Khu Phố 6, Phường Linh Trung, Quận Thủ Đức, Thành phố Hồ Chí Minh</p>
-      <p>Phone Number: 0123456789</p>
+      <p>Email: 20521381@gm.uit.edu.vn</p>
+      <p>Address: Tầng 40 Tòa E, Khu Phố 6, Phường Linh Trung, Quận Thủ Đức, Thành phố Hồ Chí Minh</p>
+      <p>Phone Number: 03939393939</p>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,13 @@ function Home() {
       <h1>Home Page</h1>
       <Carousel showArrows={true} infiniteLoop={true} autoPlay={true} interval={3000}>
         <div>
-          <img src="/kangaroo.jpg" alt="Slide 1" style={{ width: '800px', height: '500px' }} />
+          <img src="/(1).png" alt="Slide 1" style={{ width: '100%', height: 'auto' }} />
         </div>
         <div>
-        <img src="/kangaroo.jpg" alt="Slide 2" style={{ width: '800px', height: '500px' }} />
+        <img src="/(2).png" alt="Slide 2" style={{ width: '100%', height: 'auto' }} />
         </div>
         <div>
-        <img src="/kangaroo.jpg" alt="Slide 3" style={{ width: '800px', height: '500px' }} />
+        <img src="/(3).png" alt="Slide 3" style={{ width: '100%', height: 'auto' }} />
         </div>
       </Carousel>
     </div>
